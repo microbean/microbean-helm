7af03d1ad01843889c4e57392e8f360d24d74d87
Laird Nelson <ljnelson@gmail.com>
2017-10-30 11:48:33 -0700

    Fixes #51

--- src/main/java/org/microbean/helm/chart/Metadatas.java
+++ src/main/java/org/microbean/helm/chart/Metadatas.java
@@ -21,8 +21,66 @@ import java.util.Map;
 import hapi.chart.MetadataOuterClass.Maintainer;
 import hapi.chart.MetadataOuterClass.Metadata;
 
+/**
+ * A utility class for working with {@link Metadata} instances.
+ *
+ * @author <a href="https://about.me/lairdnelson"
+ * target="_parent">Laird Nelson</a>
+ *
+ * @see hapi.chart.MetadataOuterClass.Metadata.Builder
+ */
 public final class Metadatas {
 
+
+  /*
+   * Constructors.
+   */
+
+  
+  /**
+   * Creates a new {@link Metadatas}.
+   *
+   * @deprecated This constructor originally did not exist explicitly
+   * in the source code.  That means a default no-argument {@code
+   * public} constructor was available to end users.  Consequently,
+   * this constructor cannot now be made {@code private} without
+   * breaking API compatibility.  This constructor is slated for
+   * removal.
+   */
+  @Deprecated
+  public Metadatas() {
+    super();
+  }
+  
+
+  /*
+   * Static methods.
+   */
+
+
+  /**
+   * Given a {@link Map} assumed to represent a YAML document, calls
+   * certain mutating methods on the supplied {@link
+   * hapi.chart.MetadataOuterClass.Metadata.Builder}.
+   *
+   * @param metadataBuilder the {@link
+   * hapi.chart.MetadataOuterClass.Metadata.Builder} to populate; may
+   * be {@code null} in which case no action will be taken
+   *
+   * @param yamlMap a {@link Map} representing a YAML representation
+   * of a {@link Metadata}; may be {@code null} or {@linkplain
+   * Map#isEmpty() empty} in which case no action will be taken; if
+   * non-{@code null} expected to contain zero or more keys drawn from
+   * the following set: {@code annotations}, {@code apiVersion},
+   * {@code appVersion}, {@code condition}, {@code deprecated}, {@code
+   * description}, {@code engine}, {@code home}, {@code icon}, {@code
+   * keywords}, {@code maintainers}, {@code name}, {@code sources},
+   * {@code tags}, {@code tillerVersion}, {@code version}
+   *
+   * @exception ClassCastException if the supplied {@code yamlMap}
+   * does not actually represent a YAML representation of a {@link
+   * Metadata}
+   */
   public static final void populateMetadataBuilder(Metadata.Builder metadataBuilder, final Map<?, ?> yamlMap) {
     if (metadataBuilder != null && yamlMap != null && !yamlMap.isEmpty()) {
 
@@ -119,7 +177,6 @@ public final class Metadatas {
       if (version != null) {
         metadataBuilder.setVersion(version);
       }
-      
 
     }
   }
