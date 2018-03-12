f407a6aefe46f6d176cbbc8ebf06e024d672461a
Laird Nelson <ljnelson@gmail.com>
2018-03-12 16:39:59 -0700

    Fixes #127.

--- src/main/java/org/microbean/helm/package-info.java
+++ src/main/java/org/microbean/helm/package-info.java
@@ -26,7 +26,7 @@
  *
  * @see org.microbean.helm.ReleaseManager
  */
-@Version("2.8.2.1.0.0")
+@Version("2.8.2.1.0.1")
 package org.microbean.helm;
 
 import org.microbean.development.annotation.Version;
